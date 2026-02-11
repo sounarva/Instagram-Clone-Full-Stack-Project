1e57700d316972eeeccc48581144909e5fadd041
Sounarva Bardhan <sounarvabardhan2003@gmail.com>
2026-02-11 09:52:30 +0530

    Login route implemented

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -48,6 +48,51 @@ async function registerController(req, res) {
         })
 }
 
+async function loginController(req, res) {
+    const { username, email, password } = req.body
+    
+    if (!password) {
+        return res.status(400).json({
+            message: "Password is required"
+        })
+    }
+    const user = await userModel.findOne({
+        $or: [
+            {
+                username
+            },
+            {
+                email
+            }
+        ]
+    })
+    if (!user) {
+        return res.status(400).json({
+            message: "User not found"
+        })
+    }
+
+    const hashedPassword = crypto.createHash("md5").update(password).digest("hex")
+    const isPasswordValid = user.password === hashedPassword
+    if (!isPasswordValid) {
+        return res.status(401).json({
+            message: "Invalid password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET, {
+        expiresIn: "1d"
+    })
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "User logged in successfully✅"
+    })
+}
+
 module.exports = {
-    registerController
+    registerController,
+    loginController
 }
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const { registerController } = require("../controllers/user.controller")
+const { registerController , loginController } = require("../controllers/user.controller")
 
 
 router.post("/register", registerController)
+router.post("/login", loginController)
 
 module.exports = router
