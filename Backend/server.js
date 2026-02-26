4611aeeeca250e40f4bb67ad15f407a3d413b91c
Sounarva Bardhan <sounarvabardhan2003@gmail.com>
2026-02-26 15:20:10 +0530

    Deploying modifications

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const app = require('./src/app')
 const connectToDB = require('./src/config/database')
+const PORT = process.env.PORT || 3200;
 
 connectToDB()
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
--- Frontend/src/Features/Auth/Services/auth.api.js
+++ Frontend/src/Features/Auth/Services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3200/api/v1/auth',
+    baseURL: '/api/v1/auth',
     withCredentials: true
 })
 
