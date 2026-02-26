a50b7edef485f4c017ee9de1bbb769ee95dba35e
Sounarva Bardhan <sounarvabardhan2003@gmail.com>
2026-02-26 15:50:37 +0530

    Modified

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 const app = require('./src/app')
 const connectToDB = require('./src/config/database')
-const PORT = process.env.PORT || 3200;
 
 connectToDB()
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`)
 })
